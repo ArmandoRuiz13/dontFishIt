f945cacc55dc470e7239eeffcc2600e1464d4b5f
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-05-14 16:19:26 -0500

    add server.js en carpeta controller, con endpoints actuales funcionando

--- test/controllers/FizzInfoController.test.js
+++ test/controllers/FizzInfoController.test.js
@@ -2,7 +2,7 @@ const FizzInfo = require("../../lib/controllers/FizzInfoController");
 
 describe("Prueba unitaria para FizzInfoController ", () =>{
     test("Prueba 1 Imprime lista de objetos de los datos de expecies_datos package.json", () => {
-        const allinfo = FizzInfo.GetAllInfo()
+        const allinfo = FizzInfo.getAllInfo()
         expect(allinfo).not.toBeUndefined()
     });
     test("Prueba 2 Imprime lista de objetos de los datos de expecies_datos package.json por ID", () => {
